7a017b860088f733d9f48604e9e6a8d22d9de1e1
Jungeun (Jung) Hwang <89956765+jungeun202@users.noreply.github.com>
2024-06-30 12:23:58 -0400

    Update Header.jsx
    use next image

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
         <div className="container ml-2 flex justify-between items-center">
             {/* logo */}
             <Link href={"/"}>
-                <img src="./logo.png" className="flex object-cover w-20 " alt="Jung's Profile" />
+{/*                 <Image src="./logo.png" className="flex object-cover w-20 " alt="Jung's Profile" /> */}
             </Link>
             {/* desktop nav */}
             <div className="hidden xl:flex items-center gap-8">
