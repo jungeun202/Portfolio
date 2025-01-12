8ba283f1d6ede3d6424d30d7167238c92381461a
Jungeun (Jung) Hwang <89956765+jungeun202@users.noreply.github.com>
2025-01-12 15:32:05 -0600

    Update anothe project

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { BsArrowDownRight } from "react-icons/bs";
@@ -10,6 +9,40 @@ import { motion } from "framer-motion";
 const projects = [
   {
     num: '01',
+    title: 'Guess That Jam',
+    description: (
+      <>
+        Developed a scalable full-stack web game where players guess the title of a song from short audio clips, using song data retrieved from an open GitHub source.
+      </>
+    ),
+    href: 'https://github.com/jungeun202/Game-Demo',
+    skills: [
+      { icon: <SiMysql />, name: "MySQL" },
+      { icon: <SiNextdotjs />, name: "Next.js" },
+      { icon: <FaReact />, name: "React" },
+      { icon: <SiTypescript />, name: "TypeScript" },
+    ],
+    video: '/demo.mov',
+    timeline: 'Jan 2025 - Present',
+  },
+  {
+    num: '02',
+    title: 'Domain-Adaptive Learning for Visual Action Recognition with Temporal Modeling',
+    description: (
+      <>
+        Reimplemented the Recurrent Vision Transformer (RViT) architecture for a cross-modality task by tailoring the architecture for image training and video prediction, demonstrating its versatility in action label inference.
+      </>
+    ),
+    href: 'https://github.com/jungeun202/CS593',
+    skills: [
+      { icon: <SiPytorch />, name: "PyTorch" },
+      { icon: <FaOpencart />, name: "OpenCV" },
+    ],
+    image: '/proj3.png',
+    timeline: 'Aug 2024 - Dec 2024',
+  },
+  {
+    num: '03',
     title: 'PKA Web Development',
     description: (
       <>
@@ -28,7 +61,7 @@ const projects = [
     timeline: 'Jan 2024 - Aug 2024',
   },
   {
-    num: '02',
+    num: '04',
     title: 'Personal Portfolio Web Development',
     description: (
       <>
@@ -46,23 +79,7 @@ const projects = [
     timeline: 'May 2024 - Present',
   },
   {
-    num: '03',
-    title: 'Domain-Adaptive Learning for Visual Action Recognition with Temporal Modeling',
-    description: (
-      <>
-        Reimplemented the Recurrent Vision Transformer (RViT) architecture for a cross-modality task by tailoring the architecture for image training and video prediction, demonstrating its versatility in action label inference.
-      </>
-    ),
-    href: 'https://github.com/jungeun202/CS593',
-    skills: [
-      { icon: <SiPytorch />, name: "PyTorch" },
-      { icon: <FaOpencart />, name: "OpenCV" },
-    ],
-    image: '/proj3.png',
-    timeline: 'Aug 2024 - Dec 2024',
-  },
-  {
-    num: '04',
+    num: '05',
     title: 'Kaggle Competitions - LLM Classification Finetuning',
     description: (
       <>
@@ -72,7 +89,7 @@ const projects = [
     href: 'https://github.com/jungeun202/kaggle',
     skills: [
       { icon: <SiPytorch />, name: "PyTorch" },
-      { icon: <FaOpencart />, name: "OpenCV" },
+      // { icon: <FaOpencart />, name: "OpenCV" },
     ],
     image: '',
     timeline: 'Dec 2024 - Present',
@@ -80,60 +97,77 @@ const projects = [
 ];
 
 const Projects = () => {
+
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="container mx-auto">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            transition: { delay: 0.6, duration: 0.4, ease: "easeIn" },
-          }}
-          className="grid grid-cols-1 xl:grid-cols-2 gap-[60px]"
-        >
-          <div className="flex flex-col gap-12">
-            {projects.map((project, index) => (
-              <div key={index} className="flex flex-col gap-4">
-                <div className="flex items-center gap-4">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hove transition-all duration-500">
-                    {project.num}
-                  </div>
-                  <h2 className="text-[28px] font-bold leading-none text-gray-700">
-                    {project.title}
-                  </h2>
+    <div className="container mx-auto">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.6, duration: 0.4, ease: "easeIn" },
+        }}
+        className="grid grid-cols-1 xl:grid-cols-1 gap-[60px]"
+      >
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-1 xl:grid-cols-2 gap-12 items-center"
+          >
+            {/* Text Section */}
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-4">
+                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hove transition-all duration-500">
+                  {project.num}
                 </div>
-                <p className="text-gray-600">{project.description}</p>
-                <div className="flex items-center gap-4 text-gray-700">
-                  {project.skills.map((skill, i) => (
-                    <div key={i} className="relative group">
-                      <span className="text-xl">{skill.icon}</span>
-                      <span className="absolute bottom-10 left-1/2 transform -translate-x-1/2 px-2 py-1 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity">
-                        {skill.name}
-                      </span>
-                    </div>
-                  ))}
-                </div>
-                <div className="text-sm text-gray-500">{project.timeline}</div>
-                <Link href={project.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
-                  <BsArrowDownRight className="tect-primary text-2xl"/>
-                </Link> 
+                <h2 className="text-[28px] font-bold leading-none text-gray-700">
+                  {project.title}
+                </h2>
               </div>
-            ))}
-            
-          </div>
-          <div className="flex flex-col items-center gap-12">
-            {projects.map((project, index) => (
+              <p className="text-gray-600">{project.description}</p>
+              <div className="flex items-center gap-4 text-gray-700">
+                {project.skills.map((skill, i) => (
+                  <div key={i} className="relative group">
+                    <span className="text-xl">{skill.icon}</span>
+                    <span className="absolute bottom-10 left-1/2 transform -translate-x-1/2 px-2 py-1 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                      {skill.name}
+                    </span>
+                  </div>
+                ))}
+              </div>
+              <div className="text-sm text-gray-500">{project.timeline}</div>
+              <Link
+                href={project.href}
+                className="w-[50px] h-[50px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+              >
+                <BsArrowDownRight className="text-primary text-2xl" />
+              </Link>
+            </div>
+
+            {/* Media Section */}
+            <div className="w-full h-auto rounded-lg shadow-lg flex justify-center">
+              {project.video ? (
+                <video controls className="w-full h-auto">
+                  <source src={project.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              ) : project.image ? (
                 <img
-                  key={index}
                   src={project.image}
                   alt={project.title}
-                  className="w-full h-auto rounded-lg shadow-lg"
+                  className="w-full h-auto"
                 />
-              ))}
+              ) : (
+                <div className="text-center text-gray-500">
+                  No media available
+                </div>
+              )}
+            </div>
           </div>
-        </motion.div>
-      </div>
-    </section>
+        ))}
+      </motion.div>
+    </div>
+  </section>
   );
 };
 
